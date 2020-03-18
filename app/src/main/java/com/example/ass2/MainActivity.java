7c727378141fd55352f09754cc0dae3a74800274
sranj <sranjagseer@yahoo.com>
2020-03-18 01:47:22 -0400

    Assignment2

--- app/src/main/java/com/example/ass2/MainActivity.java
+++ app/src/main/java/com/example/ass2/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
@@ -14,45 +15,74 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     Spinner spinner;
-    EditText editText;
+    EditText editText = findViewById(R.id.editPrice);
     TextView textView, head, total;
     ImageView imageView;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        button = findViewById(R.id.buttonOrder);
+
         spinner = findViewById(R.id.spinner);
         imageView = findViewById(R.id.image);
-        final String str[] = {"Butter Chicken","Chicken Karai","Chicken Roll","Fish Roll","Hamburger","Junior Chicken","Egg Roll","King Meal","Mustard curry"
-                ,"Veg Thali"};
-        ArrayAdapter arrayAdapter = new ArrayAdapter(MainActivity.this,R.layout.support_simple_spinner_dropdown_item,str);
+        final String str[] = {"Butter Chicken", "Chicken Karai", "Chicken Roll", "Fish Roll", "Hamburger", "Junior Chicken", "Egg Roll", "King Meal", "Mustard curry", "Veg Thali"};
+        ArrayAdapter arrayAdapter = new ArrayAdapter(MainActivity.this, R.layout.support_simple_spinner_dropdown_item, str);
         spinner.setAdapter(arrayAdapter);
-        spinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if (str[0].equals(spinner.getItemAtPosition(1).toString())){
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                if (str[0].equals(spinner.getItemAtPosition(1).toString())) {
 
                     imageView.setImageResource(R.drawable.butter);
-                }
-                else if (str[1].equals(spinner.getItemAtPosition(1).toString())){
+
+                } else if (str[1].equals(spinner.getItemAtPosition(1).toString())) {
 
                     imageView.setImageResource(R.drawable.chickenk);
-                }
-                else if (str[2].equals(spinner.getItemAtPosition(1).toString())){
+                } else if (str[2].equals(spinner.getItemAtPosition(1).toString())) {
 
                     imageView.setImageResource(R.drawable.chicken_r);
 
-                }else if (str[3].equals(spinner.getItemAtPosition(1).toString())){
+                } else if (str[3].equals(spinner.getItemAtPosition(1).toString())) {
 
                     imageView.setImageResource(R.drawable.fishr);
+                } else if (str[4].equals(spinner.getItemAtPosition(1).toString())) {
+
+                    imageView.setImageResource(R.drawable.hamburger);
+                } else if (str[5].equals(spinner.getItemAtPosition(1).toString())) {
+
+                    imageView.setImageResource(R.drawable.juniorc);
+                } else if (str[6].equals(spinner.getItemAtPosition(1).toString())) {
+
+                    imageView.setImageResource(R.drawable.eggr);
+                } else if (str[7].equals(spinner.getItemAtPosition(1).toString())) {
+
+                    imageView.setImageResource(R.drawable.kingm);
+                } else if (str[8].equals(spinner.getItemAtPosition(1).toString())) {
+
+                    imageView.setImageResource(R.drawable.mustard);
+                } else if (str[9].equals(spinner.getItemAtPosition(1).toString())) {
+
+                    imageView.setImageResource(R.drawable.vegt);
                 }
 
+            }
+
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
 
             }
         });
+
     }
 
 
 }
+
+
+
